9b7cecc6a6e55c41b1a6e1b1fe0c25f19e212461
inf <infectionfire0911@gmail.com>
2021-12-19 14:02:05 +0300

    Changed the description. Now works with strings

--- src/main/java/org/example/functions/Features.java
+++ src/main/java/org/example/functions/Features.java
@@ -22,11 +22,17 @@ public class Features {
         String str = "";
         Element element = page.select("div.content-block").first();
         if (element!=null){
-        str+=element;
-        String[] string = str.split(">");
-        str=string[2];
-        String[] string1 = str.split("<");
-        ParserPage.features=string1[0]+"\n\n";
+//        str+=element; previous version, not work with 2+ strings
+//        String[] string = str.split(">");
+//        str=string[2];
+//        String[] string1 = str.split("<");
+//        ParserPage.features=string1[0]+"\n\n";
+            String strg = element.toString().replace("<div itemprop=\"description\" class=\"content-block\">", "");
+            strg = strg.replace("<p style=\"text-align:justify\">","");
+            strg = strg.replace("<strong>", "");
+            strg = strg.replace("</strong>","");
+            strg = strg.replace("</p>","");
+            ParserPage.features=strg+"\n";
         }else {
          ParserPage.features="\n\n";;}
         }
