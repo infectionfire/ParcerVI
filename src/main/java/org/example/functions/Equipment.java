fe5998424bfcdde3ffb9ce3bdc8d1959ad33ecb5
inf <infectionfire0911@gmail.com>
2021-12-19 16:21:29 +0300

    Equipment - create dot on the end of strings

--- src/main/java/org/example/functions/Equipment.java
+++ src/main/java/org/example/functions/Equipment.java
@@ -15,23 +15,21 @@ public class Equipment {//комплектация
 
         Element tableParameter = page.select("div.fs-13.c-gray3.complect").first();
         if (tableParameter!=null){
-        ParserPage.equipment = "<strong>Комплектация:</strong>\n\n";
-        Elements names = tableParameter.select("ul");
-        Elements values = names.select("li");
-        for (Element value : values) {
-            String theme = value.select("li").text();
-            if(theme.endsWith(";")||theme.endsWith(".")) {
-                ParserPage.equipment += "- " + theme + "\n";
-            }else{
+            ParserPage.equipment = "<strong>Комплектация:</strong>\n\n";
+            Elements names = tableParameter.select("ul");
+            Elements values = names.select("li");
+
+            for (Element value : values) {
+                String theme = value.select("li").text();
                 ParserPage.equipment += "- " + theme + ";\n";
-            }
-        }
-        if(ParserPage.equipment.endsWith(".;\n"))
-            {
-                ParserPage.equipment = ParserPage.equipment.replace(";\n",".\n");
-            }
-        ParserPage.equipment += "\n";
-        }else{
+                }
+            ParserPage.equipment = ParserPage.equipment.replace(";;",";");
+
+            StringBuilder stringBuilder = new StringBuilder(ParserPage.equipment);
+            stringBuilder.replace(stringBuilder.length()-2,stringBuilder.length()-1,".");
+            ParserPage.equipment = stringBuilder+"\n";
+            ParserPage.equipment = ParserPage.equipment.replace("..",".");
+            }else{
             ParserPage.equipment = "<strong>Комплектация:</strong>\n\n\n";
         }
     }
