02b1b28f9b3e960820cb3ce2cd9c6ec0d23f8703
infectionfire <infectionfire0911@gmail.com>
2021-12-26 21:19:09 +0300

    Optimize code for parse picture url

--- src/main/java/org/example/ApachePOIExcelWrite.java
+++ src/main/java/org/example/ApachePOIExcelWrite.java
@@ -30,7 +30,7 @@ public class ApachePOIExcelWrite {
             Cell cell = row.createCell(colNum);
             Cell cellPhoto = row.createCell(colNum+1);
             cell.setCellValue(crm);
-            cellPhoto.setCellValue(ParserPage.photos.get(index++));
+            cellPhoto.setCellValue(ParserPage.photos.get(index++).replaceAll("68x60", "800x800"));
         }
         FileOutputStream outputStream = new FileOutputStream(FILE_NAME);
         workbook.write(outputStream);
